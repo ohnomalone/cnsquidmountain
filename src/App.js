a1a0825633fe1f7dac34547e43ace2b5c9133825
Matthew Malone <m.malone@me.com>
2019-10-31 10:01:35 -0600

    Add helper to clean up data from initial word info fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {fetchWord} from './apiCalls'
+import {fetchWord} from './Utilities/apiCalls'
 
 export default class App extends React.Component {
   constructor() {
---
+++ src/Utilities/helpers.js
@@ -0,0 +1,9 @@
+export const wordFetchCleaner = (word, prefix, rootWord) => {
+    return (
+        word,
+        definition: word.results[0].definition,
+        partOfSpeech: word.results[0].partOfSpeech,
+        prefix,
+        rootWord
+    )
+}
