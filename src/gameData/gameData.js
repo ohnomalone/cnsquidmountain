bc8a52a4f43344f17eeb713298658fe4b0f784f7
Matthew Malone <m.malone@me.com>
2019-10-31 10:23:25 -0600

    Create a single game data file with game data

---
+++ src/gameData/gameData.js
@@ -0,0 +1,29 @@
+export const e_ex = {
+prefix: "e/ex",
+meaning: "out",
+wordBank : [extract, express, extend, expel, expand, eject, erupt, elapse, equip, event, evolve]
+}
+
+export const pre = {
+    prefix: "pre",
+    meaning: "before",
+    wordBank : [prebake, prejudge, pretest, preheat, prefix, pretend, prescribe, prevent, prevail, prepare, preown]
+}
+
+export const sub_sup = {
+    prefix: "sub/sup",
+    meaning: "down or under",
+    wordBank : [subtract, subway, subzero, subset, subplot, subvert, subside, submerse, suburban, subdue, submarine]
+}
+
+export const re = {
+    prefix: "re",
+    meaning: "back/again",
+    wordBank : [refill, retake, rewind, remake, report, retract, resist, repress, reform, react, reject]
+}
+
+export const pro = {
+    prefix: "pro",
+    meaning: "forward/in favor of",
+    wordBank : [propel, progress, proclaim, protect, profile, promote, protest, provide, profess, provoke, propose]
+}
