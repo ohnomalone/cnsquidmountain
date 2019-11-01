8af2dcc61d2cb62547a442c1276b5440066d7270
Matthew Malone <m.malone@me.com>
2019-11-01 09:59:03 -0600

    Create action to pass player name

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const addFetchedWords = fetchedData => ({
     type: 'ADD_FETCHED_WORDS',
     fetchedData
 })
+
+export const setUser = userName => ({
+    type: 'SET_USER',
+    userName
+})
