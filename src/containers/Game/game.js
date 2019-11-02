3a4cbfb28027d1e2bb43a3bf191d24b375b3cfad
Matthew Malone <m.malone@me.com>
2019-11-02 10:37:06 -0600

    Create header HTML structure for game container

--- src/containers/Game/game.js
+++ src/containers/Game/game.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 import './round.css'
+import squidMountainLogo from '../../images/squid_mountain_logo.png'
+
 
 export class Game extends React.Component {
     constructor() {
@@ -15,11 +17,31 @@ export class Game extends React.Component {
 
     render() {
         return (
-            <div>
+            <>
+                <header>
+                    <div className="game--logo--title">
+                    <img
+                        src={squidMountainLogo}
+                        alt="Squid Mountain Logo"
+                        className="game--logo"
+                        />
+                        <h1 className="game--title">Squid Mountain</h1>
+                    </div>
+                    <div className="game--name--startover">
+                        <h3 className="game--name__h3--text">{this.props.currentPlayer}</h3>
+                        <button
+                                type='button'
+                                className="game--startover--button"
+                                // onClick={(event) => this.handleSubmit(event)}
+                                >Start</button> 
+                    </div>
+                </header>
 
-            </div>
+            </>
         )
     }
 }
 
-export default Game
+const matStateToProps = ({ currentPlayer }) => ({ currentPlayer})
+
+export default connect(matStateToProps, null)(Game);
