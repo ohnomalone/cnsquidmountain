cead295449245990795496e83148f08094681fea
Matthew Malone <m.malone@me.com>
2019-11-02 13:03:44 -0600

    Create completed words card structure and styling

--- src/containers/Round/round.js
+++ src/containers/Round/round.js
@@ -25,6 +25,18 @@ export class Round extends React.Component {
                 </main>
                 <aside className="completed--words__aside">
                     <h2>Completed Words</h2>
+                    <div class="insertCompletedWordsHere">
+                        <container className="completedWord__container" >
+                            <div className="compltedword--word--definition">
+                                <div className="compltedword--word--pos">
+                                    <p className="completedWord__p--word">Propel</p>
+                                    <p className="completedWord__p--pos">(verb)</p>
+                                </div>
+                                <p className="completedWord__p--def">definition</p>
+                            </div>
+                                <p className="completedWord__p--sentence">The young girl often wondered how NASA was able to propel shuttles into space.</p>
+                        </container>
+                    </div>
                 </aside>
             </>
         )
