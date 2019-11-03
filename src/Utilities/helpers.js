3c8a40aca227e21fa44452a5023c54f4db1619f2
Matthew Malone <m.malone@me.com>
2019-11-02 22:31:31 -0600

    Add Id with date.now to the cleaner function

--- src/Utilities/helpers.js
+++ src/Utilities/helpers.js
@@ -4,7 +4,8 @@ export const wordFetchCleaner = (prefix, fetchedWord) => {
         definition: fetchedWord.results[0].definition,
         partOfSpeech: fetchedWord.results[0].partOfSpeech,
         prefix,
-        rootWord: createRootWord(fetchedWord.syllables.list)
+        rootWord: createRootWord(fetchedWord.syllables.list),
+        id: Date.now()
     })
 }
 
