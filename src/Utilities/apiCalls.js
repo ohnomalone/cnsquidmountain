d3b510f8b6df7ff099a64b77b6901999c50dea93
Matthew Malone <m.malone@me.com>
2019-11-04 16:53:46 -0700

    Removed API key

--- src/Utilities/apiCalls.js
+++ src/Utilities/apiCalls.js
@@ -2,7 +2,7 @@ export const fetchWord = async (word) => {
   const options = {
     method: 'GET',
     headers: {
-      'X-Mashape-Key': 'dbc73f3956mshcf26537552ba173p1081edjsn50c9347e0b06',
+      'X-Mashape-Key': 'ENTER API KEY HERE',
       'Accept': 'application/json'
     }
   }
