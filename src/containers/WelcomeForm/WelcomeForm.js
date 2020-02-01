73672d6336db705603fd1659d2a06b508b9ee89a
Matthew Malone <m.malone@me.com>
2020-02-01 13:43:19 -0700

    Change WelcomeForm component from class to functional

--- src/containers/WelcomeForm/WelcomeForm.js
+++ src/containers/WelcomeForm/WelcomeForm.js
@@ -1,48 +1,46 @@
 /* eslint-disable max-lines-per-function */
-import React from 'react'
+import React, { useState } from 'react'
 import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setPlayer, ramdomizeGameData } from '../../actions'
 import './WelcomeForm.css'
-
 import squidMountainLogo from '../../images/squid_mountain_logo.png'
 
-export class WelcomeForm extends React.Component {
-  constructor() {
-    super()
-    this.state = {
+const WelcomeForm = () => {
+  const [name, handleNameChange] = useState('')
+  // export class WelcomeForm extends React.Component {
+  //   constructor() {
+  //     super()
+  //     this.state = {
+  //       name: ''
+  //     }
+  //   }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { setPlayer } = this.props
+    setPlayer(this.state.name)
+    this.setState({
       name: ''
-    }
+    })
+
+  }
+    
+  const handleStart = () => {
+    const { ramdomizeGameData, gameData } = this.props
+    ramdomizeGameData(gameData)
+  }
+    
+  const canBeSubmitted = () => {
+    const { name } = this.state
+    return name.length > 0
   }
 
-    handleChange = (event) => {
-      this.setState({ [event.target.name]: event.target.value })
-    }
+  const isEnabled = canBeSubmitted()
 
-    handleSubmit = (event) => {
-      event.preventDefault()
-      const { setPlayer } = this.props
-      setPlayer(this.state.name)
-      this.setState({
-        name: ''
-      })
-
-    }
-    
-    handleStart = () => {
-      const { ramdomizeGameData, gameData } = this.props
-      ramdomizeGameData(gameData)
-    }
-    
-    canBeSubmitted() {
-      const { name } = this.state
-      return name.length > 0
-    }
-
-    render() {
-      const isEnabled = this.canBeSubmitted()
-      return (
+  return (
         <div className="welcomeForm__div">
           <h1 className="welcomeForm--title">Squid Mountain</h1>
           <img
@@ -51,24 +49,24 @@ export class WelcomeForm extends React.Component {
             className="large_logo"
           />
           <form className="welcomeForm">
-            <label htmlFor="name" className={this.props.currentPlayer ? 'display-none' : 'welcomeFormName__label'}>Mountaineer's Name:</label>
-            <input className={this.props.currentPlayer ? 'display-none' : 'welcomeFormName__input'} id="name" type="text" name="name" placeholder="Enter Name Here" onChange={this.handleChange} value={this.state.name} />
-            <button type="button" className={this.props.currentPlayer ? 'display-none' : 'welcomeFormName__button'} disabled={!isEnabled} onClick={this.handleSubmit}>Enter</button>
-            <p className={this.props.currentPlayer ? 'welcomeForm--welcome--text' : 'display-none'}>Welcome,</p>
-            <p className={this.props.currentPlayer ? 'currentPlayer--name' : 'display-none'}>
-              {this.props.currentPlayer}
+            <label htmlFor="name" className={props.currentPlayer ? 'display-none' : 'welcomeFormName__label'}>Mountaineer's Name:</label>
+            <input className={props.currentPlayer ? 'display-none' : 'welcomeFormName__input'} id="name" type="text" name="name" placeholder="Enter Name Here" onChange={handleChange} value={name} />
+            <button type="button" className={props.currentPlayer ? 'display-none' : 'welcomeFormName__button'} disabled={!isEnabled} onClick={handleSubmit}>Enter</button>
+            <p className={props.currentPlayer ? 'welcomeForm--welcome--text' : 'display-none'}>Welcome,</p>
+            <p className={props.currentPlayer ? 'currentPlayer--name' : 'display-none'}>
+              {props.currentPlayer}
 !
             </p>
             <div className="ready__div">
-              <p className={this.props.currentPlayer ? 'welcomeForm--ready--text' : 'display-none'}>Ready to go?</p>
-              <div className={this.props.currentPlayer ? 'start__button--wrapper' : 'display-none'}>
+              <p className={props.currentPlayer ? 'welcomeForm--ready--text' : 'display-none'}>Ready to go?</p>
+              <div className={props.currentPlayer ? 'start__button--wrapper' : 'display-none'}>
                 <div className="start__button--border">
                   <div className="start__button--pulse" />
                   <Link to="/play">
                     <button
                       type="button"
                       className="welcomeForm--start--button"
-                      onClick={this.handleStart}
+                      onClick={handleStart()}
                     >
 Start
 
@@ -79,8 +77,7 @@ Start
             </div>
           </form>
         </div>
-      )
-    }
+  )
 }
 
 const mapStateToProps = ({ currentPlayer, gameData }) => ({ currentPlayer, gameData })
