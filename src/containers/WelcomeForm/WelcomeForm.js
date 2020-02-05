06bb93f28a4e21dd304b44eff1dd430065393545
Matthew Malone <m.malone@me.com>
2020-02-04 18:51:31 -0700

    Update WelcomeForm styling

--- src/containers/WelcomeForm/WelcomeForm.js
+++ src/containers/WelcomeForm/WelcomeForm.js
@@ -35,32 +35,11 @@ const WelcomeForm = ({gameData, currentPlayer, setPlayer, ramdomizeGameData}) =>
           />
           <h1 className="welcomeForm--title">Squid Mountain</h1>
           <form className="welcomeForm">
-            <label htmlFor="name" className={currentPlayer ? 'display-none' : 'welcomeFormName__label'}>Welcome, what's your name?</label>
-            <input className={currentPlayer ? 'display-none' : 'welcomeFormName__input'} id="name" type="text" name="name" placeholder="Enter your name here!" onChange={e => handleNameChange(e.target.value)} value={name} />
-            <button type="button" className={currentPlayer ? 'display-none' : 'welcomeFormName__button'} disabled={!isEnabled} onClick={handleSubmit}>Let's Play</button>
-            <p className={currentPlayer ? 'welcomeForm--welcome--text' : 'display-none'}>Welcome,</p>
-            <p className={currentPlayer ? 'currentPlayer--name' : 'display-none'}>
-              {currentPlayer}
-!
-            </p>
-            <div className="ready__div">
-              <p className={currentPlayer ? 'welcomeForm--ready--text' : 'display-none'}>Ready to go?</p>
-              <div className={currentPlayer ? 'start__button--wrapper' : 'display-none'}>
-                <div className="start__button--border">
-                  <div className="start__button--pulse" />
-                  <Link to="/rules">
-                    <button
-                      type="button"
-                      className="welcomeForm--start--button"
-                      onClick={handleStart()}
-                    >
-Start
-
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <label htmlFor="name" className={'welcomeFormName__label'}>Welcome, what's your name?</label>
+            <input className={ 'welcomeFormName__input'} id="name" type="text" name="name" placeholder="Enter your name here!" onChange={e => handleNameChange(e.target.value)} value={name} />
+            <Link to="/rules">
+              <button type="button" className={'welcomeFormName__button'} disabled={!isEnabled} onClick={handleSubmit}>Let's Play</button>
+            </Link>
           </form>
         </div>
   )
