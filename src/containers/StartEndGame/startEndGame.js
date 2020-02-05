802d075a1007fd22af113e1a0173d18067044860
Matthew Malone <m.malone@me.com>
2020-02-05 14:39:27 -0700

    Create object with keys that are associated with the number round to show appropiate information

--- src/containers/StartEndGame/startEndGame.js
+++ src/containers/StartEndGame/startEndGame.js
@@ -5,7 +5,63 @@ import './startEndGame.css'
 import squidMountainLogo from '../../images/squid_mountain_logo.png'
 
 
-const StartEndGame = ({ currentPlayer }) => {
+const StartEndGame = ({ currentPlayer, currentRound }) => {
+
+    // create a variable that takes in the currentRound and assignes the correct thing for the object to be returned
+    // create object that holds the three options for what to return
+    const showRules = {
+        0 : (
+            <div className="startEndGame__container">
+            <div className="startEndGame__main">
+                <h2 className="startEndGame--welcome">
+                    Hello, Mountainer
+                {' '}
+                {currentPlayer}
+        !
+                </h2>
+                <p className="rules-welcome__p">Are you ready to warm up before you climb Squid Mountain?</p>
+                <div className="rules__container">
+                    <p className="rules__p">Rules:</p>
+                    <ol>
+                        <li className="rules__p">Match the prefix on the left with  <br /> it's meaning on the right</li>
+                        <li className="rules__p">A correct match turns green and will <br /> show in the completed column on the right</li>
+                        <li className="rules__p">An incorret match will turn red and allow  <br /> you to try again</li>
+                    </ol>
+
+                </div>
+                <Link to="/play">
+                <button type="button" className={'welcomeFormName__button'}>Start Warm Up</button>
+                </Link>
+            </div>
+        </div>
+        ),
+        1: (
+            <div className="startEndGame__container">
+            <div className="startEndGame__main">
+                <h2 className="startEndGame--welcome">
+                    Great warmup,
+                {' '}
+                {currentPlayer}
+        !
+                </h2>
+                <p className="rules-welcome__p">With that warmup you're ready to climb Squid Mountain!</p>
+                <div className="rules__container">
+                    <p className="rules__p">Rules:</p>
+                    <ol>
+                        <li className="rules__p">Make a complete word by matching the prefix  <br /> on the left with the root on the right </li>
+                        <li className="rules__p">A correct match turns green and will <br /> show in the completed column on the right</li>
+                        <li className="rules__p">An incorret match will turn red and allow  <br /> you to try again</li>
+                    </ol>
+                </div>
+                <Link to="/play">
+                <button type="button" className={'welcomeFormName__button'}>Let's Climb</button>
+                </Link>
+            </div>
+        </div>
+        )
+    }
+
+    
     return (
     <section className="startEndGame__section">
         <header className="header">
@@ -16,29 +72,7 @@ const StartEndGame = ({ currentPlayer }) => {
         />
         <h3 className="game--title">Squid Mountain</h3>
         </header>
-        <div className="startEndGame__container">
-        <div className="startEndGame__main">
-            <h2 className="startEndGame--welcome">
-                Hello, Mountainer
-            {' '}
-            {currentPlayer}
-    !
-            </h2>
-            <p className="rules-welcome__p">Are you ready to warm up before you climb Squid Mountain?</p>
-            <div className="rules__container">
-                <p className="rules__p">Rules:</p>
-                <ol>
-                    <li className="rules__p">Match the prefix on the left with  <br/> it's meaning on the right</li>
-                    <li className="rules__p">A correct match turns green and will <br/> show in the completed column on the right</li>
-                    <li className="rules__p">An incorret match will turn red and allow  <br/> you to try again</li>
-                </ol>
-
-            </div>
-            <Link to="/play">
-            <button type="button" className={'welcomeFormName__button'}>Start Warm Up</button>
-            </Link>
-        </div>
-        </div>
+        {showRules[1]}
     </section>
     )
     }
